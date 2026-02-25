bba012004c02c8a219bdbf727d324fd8e33a3440
thailakeita <thailakeita@localhost>
2026-02-25 01:52:24 +0100

    Ajout de la classe Joueur conforme au diagramme des classes

---
+++ MonopSchool/src/main/java/fr/idmc/monopschool/MonopSchool.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.idmc.monopschool;
+
+/**
+ *
+ * @author thailakeita
+ */
+public class MonopSchool {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
